03af22872d8f3dbec48876540a8a1bb07b2c9c62
Douglas Frari <douglas.frari@cesar.org.br>
2015-09-11 19:27:06 -0300

    versao do projeto em construcao na disciplina de POO 2015.2

---
+++ SistemaVenda/src/sistemavenda/Teste.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemavenda;
+
+import telas.TelaPrincipal;
+
+/**
+ *
+ * @author Douglas
+ */
+public class Teste {
+    
+    public static void main(String[] args) {
+        System.out.println("Testando main()");
+     
+        TelaPrincipal tela = new TelaPrincipal();
+        
+    }
+    
+}
