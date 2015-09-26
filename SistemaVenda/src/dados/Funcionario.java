53432177e5400aa135741a8d7ad770ab280d3324
alunofavip <alunofavip@192.168.60.155>
2015-09-25 22:00:31 -0300

    Minha Versão Atual Da Avalicao 1.1

--- SistemaVenda/src/dados/Funcionario.java
+++ SistemaVenda/src/dados/Funcionario.java
@@ -16,7 +16,83 @@ public class Funcionario {
  private String telefone;
  private String cpf;
  private String funcao;
+ private int codigo;
+ private static int contador = 1;
  
+ public Funcionario(String nome,String email,String telefone,String cpf,String funcao){
+   this.codigo = contador;
+   contador++;
+   this.nome = nome;
+   this.cpf = cpf;
+   this.email = email;
+   this.telefone = telefone;
+   this.funcao = funcao;
+    
+ }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        Funcionario.contador = contador;
+    }
+
+    @Override
+    public String toString() {
+    String relatorio = ("\nNome: "+nome+"\nCpf: "+cpf+"\nEmail:"+email+"\nCódigo"+codigo+"\nTelefone: "+telefone+"\nFunção: "+funcao);
+    return relatorio ;
+    
+    }
  
+
     
 }
